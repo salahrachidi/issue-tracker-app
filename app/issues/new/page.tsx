369133a36a274357e9a7fc7f4863649bacdfd29a
salahrachidi <salah.ra770@gmail.com>
2025-10-28 16:50:21 +0100

    handling form submition using react-hook-form, and sending data to back-end using axios

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,38 @@
 //!Building a form needs user interaction so it must not be render on the server
 'use client';
 import { Button, TextField } from	'@radix-ui/themes'
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'
+import { useForm, Controller } from 'react-hook-form'
+import	axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 const SimpleMdeReact = dynamic(() => import('react-simplemde-editor'), {
 	ssr: false,
 });
 
 
+interface	IssueForm {
+	title:	string;
+	description: string;
+}
+
 const NewIssuePage = () => {
+	const router = useRouter();
+	const { register, control, handleSubmit } = useForm<IssueForm>();
+	const onSubmit = handleSubmit(async (data) => {
+		await axios.post('/api/issues', data);
+		router.push('/issues');
+	});
   return (
-	<div className='max-w-xl space-y-3'>
-		<TextField.Root placeholder='Title' />
-		<SimpleMdeReact placeholder="description" />
-		<Button>Submit new issue</Button>
-	</div>
+	<form className='max-w-xl space-y-3' onSubmit={onSubmit}>
+		<TextField.Root placeholder='Title' {...register('title')} />
+		<Controller 
+			name="description"
+			control={control}
+			render={({ field }) => <SimpleMdeReact placeholder="description" {...field}/>}
+		/>
+		<Button type="submit">Submit new issue</Button>
+	</form>
   )
 }
 
