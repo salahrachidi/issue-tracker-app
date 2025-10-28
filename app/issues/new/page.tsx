f2b4b7a2701ef4bb342563bd9c227271a5175dd0
salahrachidi <salah.ra770@gmail.com>
2025-10-28 15:56:04 +0100

    instal and use simplemde react editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,18 @@
 //!Building a form needs user interaction so it must not be render on the server
 'use client';
-import { Button, TextArea, TextField } from	'@radix-ui/themes'
-import React from 'react'
+import { Button, TextField } from	'@radix-ui/themes'
+import dynamic from 'next/dynamic';
+
+const SimpleMdeReact = dynamic(() => import('react-simplemde-editor'), {
+	ssr: false,
+});
 
 
 const NewIssuePage = () => {
   return (
 	<div className='max-w-xl space-y-3'>
 		<TextField.Root placeholder='Title' />
-		<TextArea placeholder="description" />
+		<SimpleMdeReact placeholder="description" />
 		<Button>Submit new issue</Button>
 	</div>
   )
