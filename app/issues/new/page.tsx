e05c7fdbf4e98a9813fc3c878ab6bc820eb646cf
salahrachidi <salah.ra770@gmail.com>
2025-10-28 17:26:21 +0100

    customize zod error messages and display them with RadixUI Callout components

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 //!Building a form needs user interaction so it must not be render on the server
 'use client';
-import { Button, TextField } from	'@radix-ui/themes'
+import { Button, Callout, TextField } from	'@radix-ui/themes'
 import dynamic from 'next/dynamic'
 import { useForm, Controller } from 'react-hook-form'
 import	axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const SimpleMdeReact = dynamic(() => import('react-simplemde-editor'), {
 	ssr: false,
@@ -17,22 +18,32 @@ interface	IssueForm {
 }
 
 const NewIssuePage = () => {
+	const[error, setError] = useState('');
 	const router = useRouter();
 	const { register, control, handleSubmit } = useForm<IssueForm>();
 	const onSubmit = handleSubmit(async (data) => {
-		await axios.post('/api/issues', data);
-		router.push('/issues');
+		try {
+			await axios.post('/api/issues', data);
+			router.push('/issues');
+		} catch (error) {
+			setError('An unexpected error occurred.');
+		}
 	});
   return (
-	<form className='max-w-xl space-y-3' onSubmit={onSubmit}>
-		<TextField.Root placeholder='Title' {...register('title')} />
-		<Controller 
-			name="description"
-			control={control}
-			render={({ field }) => <SimpleMdeReact placeholder="description" {...field}/>}
-		/>
-		<Button type="submit">Submit new issue</Button>
-	</form>
+	<div className='max-w-xl'>
+		{error && <Callout.Root color='red' className='mb-5'>
+			<Callout.Text>{error}</Callout.Text>
+			</Callout.Root>}
+		<form className=' space-y-3' onSubmit={onSubmit}>
+			<TextField.Root placeholder='Title' {...register('title')} />
+			<Controller
+				name="description"
+				control={control}
+				render={({ field }) => <SimpleMdeReact placeholder="description" {...field}/>}
+			/>
+			<Button type="submit">Submit new issue</Button>
+		</form>
+	</div>
   )
 }
 
